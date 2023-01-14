abe4ebe0f69fe768cc1d0ad3c2230d89b1dcf866
Ahmad Bayrakdar <ahmad.berkdar.sy@gmail.com>
2023-01-14 13:28:41 +0200

    created cart component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import TopNav from './components/TopNav';
 import SearchBarContainer from './components/SearchBarContainer';
 import Varities from './components/Varities';
 import Footer from './components/Footer';
+import Cart from './components/Cart';
 
 import ProductContent from './components/ProductContent';
 
 function App() {
   return (
-    <div>
+    <div className='appContainer'>
       <TopNav />
       <SearchBarContainer />
       <Varities />
       <ProductContent />
       <Footer />
+      <Cart />
     </div>
   );
 }
---
+++ src/components/Cart.js
@@ -0,0 +1,16 @@
+import '../styles/Cart.scss';
+
+function Cart() {
+    return(
+        <div className="cartContainer">
+            <div className="cartBackground">
+                cartHere
+            </div>
+            <div className="myCart">
+                <h2>My Cart</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Cart;
